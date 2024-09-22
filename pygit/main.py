f312fc8d741db33a9f1de40762eee3d584c35fe0
Max Celant <massimiliano.celant@aa.com>
2024-09-22 12:51:20 -0500

    getting args

--- pygit/main.py
+++ pygit/main.py
@@ -2,8 +2,17 @@ import sys
 
 USAGE = 'Usage: pygit <command> [<args>]'
 
-def main():
+def usage():
   print(USAGE)
+  exit(1)
+
+def main():
+  try:
+    cmd, args = sys.argv[1], sys.argv[2:] if sys.argv[2] else None
+    print(cmd)
+    print(args)
+  except:  
+    usage()
 
 if __name__ == '__main__':
   main()
